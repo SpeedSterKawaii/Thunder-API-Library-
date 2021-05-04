d1fc74af4882a6e9f6c02a16aab80f54a756c4d5
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-05-03 21:15:37 -0400

    Create ThunderHub.lua

---
+++ ThunderHub.lua
@@ -0,0 +1,28 @@
+-- Thunder Hub by SpeedSterKawaii --
+-- You are not allowed to skid this --
+warn("Thunder Hub - Script Executed")
+
+local Effect  = Instance.new("BlurEffect") -- Blur Effect
+Effect.Parent = game.Lighting
+
+local ThunderLoading = Instance.new("ScreenGui")
+local Logo = Instance.new("ImageLabel")
+
+ThunderLoading.Name = "ThunderLoading"
+ThunderLoading.Parent = game.CoreGui 
+ThunderLoading.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Logo.Name = "Logo" -
+Logo.Parent = ThunderLoading
+Logo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Logo.BackgroundTransparency = 1.000
+Logo.Position = UDim2.new(0.427343756, 0, 0.396386832, 0)
+Logo.Size = UDim2.new(0, 186, 0, 194)
+Logo.Image = "rbxassetid://6764952705"
+Logo.ScaleType = Enum.ScaleType.Fit
+wait(3)
+for i = 1, 10 do
+    Logo.ImageTransparency = Logo.ImageTransparency + 0.1
+    Effect:Destroy()
+    wait()
+end
